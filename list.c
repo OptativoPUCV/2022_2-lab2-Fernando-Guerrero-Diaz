540237deac5be6e21d45a3e061c06e9a47d83ea8
Replit user <>
2022-08-19 16:37:06 +0000

    new try

--- list.c
+++ list.c
@@ -45,14 +45,13 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-    if (list->current==list->tail){
+    if (list->current->next){
+      list->current=list->current->next;
+      return list->current->data;
+    }
+    else {
       return NULL;
     }
-    list->current = list->current->next;
-    if (list->current->next == NULL){
-      list->tail =list->current;
-    }
-    return list->current->data;
 }
 
 void * lastList(List * list) {
